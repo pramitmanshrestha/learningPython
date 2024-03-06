c6b678d457f54852fcb50b1fa843f277e649c3a3
Bibek Amatya <bibek.amatya@benefitmall.com>
2024-03-05 20:29:40 -0600

    changes

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -8,5 +8,5 @@ def main():
     name = input("What is your name?")
     print("Nice to meet you",name)
 
-if __name__ == "__main___":
+if __name__ == "__main__":
     main()
--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -13,25 +13,42 @@ mylist = [0, 1, "two", 3.2, False]
 mytuple = (0, 1, 2)
 mydict = {"one" : 1, "two" : 2}
 
-print(myint)
-print(myfloat)
-print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
+# print(myint)
+# print(myfloat)
+# print(mystr)
+# print(mybool)
+# print(mylist)
+# print(mytuple)
+# print(mydict)
 
 # re-declaring a variable works
+myint = "abc"
+# print(myint)
 
 # to access a member of a sequence type, use []
+# print(mylist[2])
 
-# use slices to get parts of a sequence
+# # use slices to get parts of a sequence
+# print(mylist[1:5])
+# print(mylist[1:5:2])
 
-# you can use slices to reverse a sequence
+# # you can use slices to reverse a sequence
+# print(mylist[::-1])
 
-# dictionaries are accessed via keys
+# # dictionaries are accessed via keys
+# print(mydict["two"])
 
-# ERROR: variables of different types cannot be combined
+# # ERROR: variables of different types cannot be combined
+# print("string tyo" + str(123))
 
 # Global vs. local variables in functions
 
+def somefunction():
+    global mystr
+    mystr="def"
+    print(mystr)
+somefunction()
+print(mystr)
+
+del mystr
+print(mystr)
